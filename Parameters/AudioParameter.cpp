484ea59ef416c3ed993ffd5949100a64ba54d0d9
matt-twitch <matthewtwitchen@me.com>
2024-01-22 20:40:59 +0000

    added skeleton to AudioParameter.cpp

---
+++ Parameters/AudioParameter.cpp
@@ -0,0 +1,25 @@
+#include "AudioParameter.h"
+
+template <class type>
+void AudioParameter<type>::init()
+{
+
+}
+
+template <class type>
+void AudioParameter<type>::tick()
+{
+
+}
+
+template <class type>
+void AudioParameter<type>::process()
+{
+
+}
+
+template <class type>
+type AudioParameter<type>::getValue()
+{
+
+}
