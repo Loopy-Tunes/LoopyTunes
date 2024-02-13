b1a14c3e9a6d0bcc4ca86d58227a1bb018925c50
matt-twitch <matthewtwitchen@me.com>
2024-02-13 15:30:46 +0000

    added clear buffer

--- DSP/Track.h
+++ DSP/Track.h
@@ -16,6 +16,7 @@ public:
 
     void init(daisy::DaisySeed* seed, float* mem[2], DelayLine<float, MAXDELAY>* dl[2], dsy_gpio_pin r, dsy_gpio_pin p);
     void tick();
+    void clearBuffer();
 
     void setIsRecording();
     void setIsPlaying();
