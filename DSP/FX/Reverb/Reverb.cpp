7ec3d3a3669a08b3cf281a541cd6c74b7af9b858
matt-twitch <matthewtwitchen@me.com>
2024-03-04 12:47:45 +0000

    added reverb skeleton header

---
+++ DSP/FX/Reverb/Reverb.cpp
@@ -0,0 +1,26 @@
+#include "Reverb.h"
+
+void Reverb::init(DaisySeed* seed)
+{
+
+}
+
+void Reverb::process()
+{
+
+}
+
+void Reverb::processReplacing()
+{
+
+}
+
+void Reverb::suspend()
+{
+
+}
+
+void Reverb::resume()
+{
+
+}
