2bc1ea4b76e57beca157999003abdefda17012b5
matt-twitch <matthewtwitchen@me.com>
2024-03-04 16:27:12 +0000

    added reverb param initialisations

--- DSP/FX/Reverb/Reverb.cpp
+++ DSP/FX/Reverb/Reverb.cpp
@@ -2,7 +2,12 @@
 
 void Reverb::init(DaisySeed* seed)
 {
-    
+    mode.init(seed, 0, 1, LINEAR, ChannelIDs::ENCODER, [this] (float m) { model.setmode(m); });
+    size.init(seed, 0, 1, LINEAR, ChannelIDs::ENCODER, [this] (float s) { model.setroomsize(s); });
+    damp.init(seed, 0, 1, LINEAR, ChannelIDs::ENCODER, [this] (float d) { model.setdamp(d); });
+    wet.init(seed, 0, 1, LINEAR, ChannelIDs::ENCODER, [this] (float w) { model.setwet(w); });
+    dry.init(seed, 0, 1, LINEAR, ChannelIDs::ENCODER, [this] (float d) { model.setdry(d); });
+    width.init(seed, 0, 1, LINEAR, ChannelIDs::ENCODER, [this] (float w) { model.setwidth(w); });
 }
 
 void Reverb::process()
