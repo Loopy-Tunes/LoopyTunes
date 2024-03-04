7fe839be475708c6aca6d94930290f7dc429352c
matt-twitch <matthewtwitchen@me.com>
2024-03-04 12:47:45 +0000

    added reverb skeleton header

---
+++ DSP/FX/Reverb/Reverb.h
@@ -0,0 +1,39 @@
+#ifndef REVERB_H
+#define REVERB_H
+
+#include "../../../Parameters/AudioParameter.h"
+#include "../../../Parameters/BinaryParameter.h"
+
+/*********************************************************************//**
+ *  Class name: Reverb
+ *  Function: Processes reverb effect for block of input sanples
+ * 
+ *  Port of the open-source Freeverb algorithm by Jezar (Dreampoint, 2000)
+ *  https://github.com/sinshu/freeverb
+ ************************************************************************/
+using namespace daisysp;
+
+class Reverb
+{
+public:
+
+    void init(DaisySeed* seed);
+    void process();
+    void processReplacing();
+    void suspend();
+    void resume();
+
+private:
+
+    // model
+
+    BinaryParameterWrapper bypass;
+    AudioParameterWrapper<float> mode;
+    AudioParameterWrapper<float> size;
+    AudioParameter<float> damp;
+    AudioParameter<float> wet;
+    AudioParameter<float> dry;
+    AudioParameter<float> width;
+};
+
+#endif
