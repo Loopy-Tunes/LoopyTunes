5a53a51dc1b9975ed42eb09c34279f7ed812dcc5
matt-twitch <matthewtwitchen@me.com>
2024-04-17 14:17:04 +0100

    before class separations

--- Drivers/LcdBuffers.h
+++ Drivers/LcdBuffers.h
@@ -4,7 +4,7 @@
 #include "daisy_seed.h"
 
 // Global buffers for LCD
-extern uint8_t DMA_BUFFER_MEM_SECTION frame_buffer[153600];
-extern uint8_t DSY_SDRAM_BSS          color_mem[153600 / 2];
+//extern uint8_t DMA_BUFFER_MEM_SECTION frame_buffer[153600];
+//extern uint8_t DSY_SDRAM_BSS          color_mem[153600 / 2];
 
 #endif 
--- Drivers/daisy_ILI9341.h
+++ Drivers/daisy_ILI9341.h
@@ -107,55 +107,9 @@ class ILI9341SpiTransport
 
     // an internal function to handle SPI DMA callbacks
     // called when an DMA transmission completes and the next driver must be updated
-    static void TxCompleteCallback(void* context, SpiHandle::Result result)
-    {
-        if(result == SpiHandle::Result::OK)
-        {
-            auto transport     = static_cast<ILI9341SpiTransport*>(context);
-            auto transfer_size = transport->GetTransferSize();
-            transport->remaining_buff -= transfer_size;
-            if(transport->remaining_buff > 0)
-            {
-                uint8_t* next_buffer_ptr = frame_buffer + (transport->buffer_size - transport->remaining_buff);  // potential issue
-                transport->SendDataDMA(
-                   &frame_buffer[buffer_size - transport->remaining_buff],
-                    transfer_size);
-                    
+    static void TxCompleteCallback(void* context, SpiHandle::Result result);
 
-                // 16bit transfer             uncommented
-                // transport->SendDataDMA(
-                  //   &frame_buffer[2 * transport->buf_chunk_size],
-                    // transport->buf_chunk_size);
-            }
-            else
-            {
-                //System::Delay(120);
-                transport->dma_busy = false;
-                
-
-                // 8 bit transfer
-                // auto* spi_h = transport->spi_.GetHandle();
-                // spi_h->Instance->CFG1 &= ~SPI_CFG1_DSIZE_3;
-                // spi_h->Instance->CFG2 &= ~SPI_CFG2_LSBFRST;
-                // spi_h->Init.DataSize = SPI_DATASIZE_8BIT;
-            }
-        }
-    }
-
-    SpiHandle::Result SendDataDMA()
-    {
-        remaining_buff = buffer_size;
-        //System::Delay(120);
-        dma_busy       = true;
-        
-
-        // auto* spi_h = spi_.GetHandle();
-        // spi_h->Instance->CFG1 |= SPI_CFG1_DSIZE_3;
-        // spi_h->Instance->CFG2 |= SPI_CFG2_LSBFRST;
-        // spi_h->Init.DataSize = SPI_DATASIZE_16BIT; // 15;
-
-        return SendDataDMA(frame_buffer, buf_chunk_size);
-    };
+    SpiHandle::Result SendDataDMA();
 
     SpiHandle::Result SendDataDMA(uint8_t* buff, size_t size)
     {
@@ -209,25 +163,9 @@ class ILI9341SpiTransport
         SendCommand(0x2C); // RAMWR
     }
 
-    void PaintPixel(uint32_t id, uint8_t color_id, uint8_t alpha = 255) const
-    {
-        auto color = tftPalette[color_id];
+    void PaintPixel(uint32_t id, uint8_t color_id, uint8_t alpha = 255) const;
 
-        if(alpha != 255)
-        {
-            
-
-            auto bg_color = tftPalette[color_mem[id]];
-            color         = Blend565(color, bg_color, alpha);
-        }
-
-        color_mem[id] = color_id; 
-
-        frame_buffer[id]     = color >> 8;
-        frame_buffer[id + 1] = color & 0xFF;
-    }
-
-    uint16_t GetPixel(uint32_t id) { return color_mem[id]; }
+    uint16_t GetPixel(uint32_t id);
 
     bool     dma_busy       = false;
     uint32_t remaining_buff = 0;
